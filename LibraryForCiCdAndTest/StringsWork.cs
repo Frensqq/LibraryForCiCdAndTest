4889d8fead29a2ec1bcaa2daba722a4b51441e4c
Frensqq <liadovn@yandex.ru>
2026-05-11 21:23:14 +0300

    complete methods

---
+++ LibraryForCiCdAndTest/StringsWork.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryForCiCdAndTest
+{
+    public class StringsWork
+    {
+
+        public bool SearchInString(string input, string search)
+        {
+            if (search == "") {
+                throw new ArgumentNullException("search is Null");
+            }
+            if (input == "") {
+                throw new ArgumentNullException("input is Null");
+            }
+            bool result = false;
+
+            try
+            {
+                result = input.Contains(search);
+            }
+            catch (Exception ex) {
+                Debug.WriteLine(ex);
+                Trace.WriteLine(ex.ToString());
+            }
+           
+            return result;
+        }
+
+        public void ChangeInString(string input, string search, string str)
+        {
+            if (search == "")
+            {
+                throw new ArgumentNullException("search is Null");
+            }
+            if (input == "")
+            {
+                throw new ArgumentNullException("input is Null");
+            }
+            if (str == "")
+            {
+                throw new ArgumentNullException("str is Null");
+            }
+            bool result = false;
+
+            if (SearchInString(input, search))
+            {
+                input.Replace(search, str);
+            }
+            else {
+                
+            }
+        }
+
+        public string RemoveSpaces(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+            return text.Replace(" ", "");
+        }
+    }
+}
